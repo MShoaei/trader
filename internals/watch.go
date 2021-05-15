557f4486aaa22fa5d8caa0c385527afc8762cc58
Mohammad Shoaei <mohammad.shoaei@outlook.com>
2021-05-16 01:36:45 +0430

    bug fix

--- internals/watch.go
+++ internals/watch.go
@@ -112,6 +112,7 @@ func (w *Watchdog) Watch(client *binance.Client) (binance.WsKlineHandler, binanc
 					Side(binance.SideTypeBuy).
 					Type(binance.OrderTypeLimit).
 					Quantity(quantity.String()).
+					TimeInForce(binance.TimeInForceTypeGTC).
 					Price(newCandle.ClosePrice.String()).Do(context.Background())
 				if err != nil {
 					log.Fatalf("sell failed: %v", err)
